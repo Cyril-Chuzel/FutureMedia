7b41490dfa743176d52cc862d047a1f97cebe1f2
Cyril CHUZEL <60505014+Cyril-Chuzel@users.noreply.github.com>
2023-04-17 10:30:26 +0200

    Updated graphic

--- sketch.js
+++ sketch.js
@@ -1,61 +1,69 @@
-let circleSizes = [];
-let circleDangerLevels = [];
+let data;
+let maxDiameter;
+let numCircles = 6; // nombre de cercles
+let circleRadii = [400,500,900,600,700,800]; // rayons des cercles
+let circleAngles = [0, 0,0,0,0,0]; // angles des cercles en orbite
+let circleSpeeds = [0.005, 0.005, 0.005,0.005,0.005,0.005]; // vitesses de rotation des cercles en orbite
+
+function preload() {
+  // Charger le fichier CSV
+  data = loadTable('risklist.csv', 'csv', 'header');
+}
 
 function setup() {
-  createCanvas(700, 700);
-  for (let i = 0; i < 40; i++) {
-    circleSizes.push(random(5, 25)); // stocker les tailles générées aléatoirement
-    circleDangerLevels.push(floor(random(6))); // stocker les niveaux de danger aléatoires (0-5)
+  // Créer un canvas de 1000x1000 pixels
+  createCanvas(1800, 1800);
+
+  // Fond gris
+  background(200);
+
+  // Trouver la plus grande valeur dans la colonne "Diameter"
+  maxDiameter = max(data.getColumn('Diameter'));
+
+  // Dessiner les cercles
+  noFill();
+  stroke(0);
+  strokeWeight(2);
+  for (let i = 0; i < numCircles; i++) {
+    ellipse(width/2, height/2, circleRadii[i]*2);
   }
 }
 
 function draw() {
-  background(220);
+  // Effacer le canvas à chaque frame
+  background(200);
+
+  // Dessiner les cercles principaux
   noFill();
   stroke(0);
   strokeWeight(2);
-  
-  // dessiner le cercle au centre
-  ellipse(width/2, height/2, 100, 100);
-  
-  // dessiner les cercles concentriques
-  for (let i = 1; i <= 5; i++) {
-    let r = i * 40;
-    noFill();
-    stroke(0);
-    strokeWeight(2);
-    if (i == 1) { // si on est au premier cercle
-      fill(0, 255, 0); // colorier en vert
-      noStroke();
-    }
-    ellipse(width/2, height/2, r*2, r*2);
-    
-    // dessiner les cercles en orbite
-    let numOrbit = i * 8;
-    let orbitRadius = r + 10;
-    let orbitSpeed = map(i, 1, 5, 0.05, 0.01);
-    for (let j = 0; j < numOrbit; j++) {
-      let x = width/2 + orbitRadius * cos(j * TWO_PI/numOrbit + millis() * orbitSpeed/1000);
-      let y = height/2 + orbitRadius * sin(j * TWO_PI/numOrbit + millis() * orbitSpeed/1000);
-      let dangerLevel = circleDangerLevels[j % circleDangerLevels.length]; // sélectionner un niveau de danger aléatoire dans le tableau
-      if (dangerLevel == 0) {
-        fill(0, 255, 0); // vert si sans danger
-      } else if (dangerLevel == 1 || dangerLevel == 2) {
-        fill(255, 165, 0); // orange si moyen danger
-      } else {
-        fill(255, 0, 0); // rouge si danger élevé
-      }
-      let size = circleSizes[j % circleSizes.length]; // sélectionner une taille aléatoire dans le tableau
-      ellipse(x, y, size, size);
-    }
+  for (let i = 0; i < numCircles; i++) {
+    let x = width/2;
+    let y = height/2;
+    ellipse(x, y, circleRadii[i]*2);
   }
-  
-  // changer les tailles et les niveaux de danger aléatoirement
-  if (frameCount  == 300) { // toutes les 60 frames
-    for (let i = 0; i < circleSizes.length; i++) {
-      circleSizes[i] = random(5, 25); // générer de nouvelles tailles aléatoires
-      circleDangerLevels[i] = floor(random(6)); // générer de nouveaux niveaux de danger aléatoires (0-5)
-    }
+
+  // Dessiner les cercles en orbite autour des cercles principaux
+  noStroke();
+  fill(0);
+  for (let i = 0; i < data.getRowCount(); i++) {
+    let diameter = data.getNum(i, 'Diameter');
+    let radius = map(diameter, 0, maxDiameter, 10, 100);
+
+    // Calculer la position en x et y du cercle sur l'orbite
+    let circleIndex = i % numCircles; // index du cercle sur lequel le cercle doit être placé
+    let circleRadius = circleRadii[circleIndex]; // rayon du cercle sur lequel le cercle doit être placé
+    let angle = map(i, 0, data.getRowCount(), 0, TWO_PI); // angle sur l'orbite
+    angle += circleAngles[circleIndex]; // ajouter l'angle de rotation du cercle en orbite
+    let x = width/2 + circleRadius * cos(angle);
+    let y = height/2 + circleRadius * sin(angle);
+
+    // Dessiner le cercle
+    ellipse(x, y, radius);
   }
-  fullscreen();
+
+  // Mettre à jour les angles de rotation des cercles en orbite
+  for (let i = 0; i < numCircles; i++) {
+    circleAngles[i] += circleSpeeds[i];
+  }
 }
