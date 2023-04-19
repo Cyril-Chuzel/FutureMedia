4c65bfe29fc7f2b4b2b25c0e671d1337553202bd
Cyril CHUZEL <60505014+Cyril-Chuzel@users.noreply.github.com>
2023-04-19 20:37:43 +0200

    debug

--- sketch.js
+++ sketch.js
@@ -64,90 +64,73 @@ function toggleCircles() {
 
 function draw() {
   
- // Placer l'image de légende
- img.position(20,150); 
- img.size(200,248);
-  
-  // Relance le mouvement des bulles à chaque frames s'il n'est pas actuellement bloqué
-  if (circlesStopped != true) {
-    circleSpeeds = [0.005, 0.0035, 0.0025, 0.002, 0.0015, 0.001, 0.0008, 0.0006, 0.0004,0.0003]; 
-  } 
-  
-  background(50);
+   // Placer l'image de légende
+   img.position(20,150); 
+   img.size(200,248);
 
-  //Afficher le titre
-  textSize(40);
-  textAlign(CENTER);
-  fill(255);
-  text("The end of the world is not today",700,300);
-  
-  
-  // Dessiner les bulles sur les orbites
-  for (let i = 0; i < data.getRowCount(); i++) {
-    let diameter = data.getNum(i, 'Diameter');
-    let radius = map(diameter, 0, maxDiameter,10,30);
+    // Relance le mouvement des bulles à chaque frames s'il n'est pas actuellement bloqué
+    if (circlesStopped != true) {
+      circleSpeeds = [0.005, 0.0035, 0.0025, 0.002, 0.0015, 0.001, 0.0008, 0.0006, 0.0004,0.0003]; 
+    } 
 
-    // Calculer la position en x et y du cercle sur l'orbite
-    let circleIndex = i % numCircles; // index du cercle sur lequel le cercle doit être placé
-    let circleRadius = circleRadii[circleIndex]; // rayon du cercle sur lequel le cercle doit être placé
-    let angle = map(i, 0, data.getRowCount(), 0, TWO_PI); // angle sur l'orbite
-    angle += circleAngles[circleIndex]; // ajouter l'angle de rotation du cercle en orbite
+    background(50);
+
+    //Afficher le titre
+    textSize(40);
+    textAlign(CENTER);
+    fill(255);
+    text("The end of the world is not today",700,300);
+
+
+    // Dessiner les bulles sur les orbites
+    for (let i = 0; i < data.getRowCount(); i++) {
+      let diameter = data.getNum(i, 'Diameter');
+      let radius = map(diameter, 0, maxDiameter,10,30);
+
+      // Calculer la position en x et y du cercle sur l'orbite
+      let circleIndex = i % numCircles; // index du cercle sur lequel le cercle doit être placé
+      let circleRadius = circleRadii[circleIndex]; // rayon du cercle sur lequel le cercle doit être placé
+      let angle = map(i, 0, data.getRowCount(), 0, TWO_PI); // angle sur l'orbite
+      angle += circleAngles[circleIndex]; // ajouter l'angle de rotation du cercle en orbite
       let x = width/2 + (circleRadius + addedX) * cos(angle)/(0.5 + addedX);
       let y = height/2 + (circleRadius + addedY) * sin(angle)/(1.5 - addedY);
 
-    // Définir la couleur en fonction de l'IP Max
-    let ipMax = data.getNum(i, 'IP');
-      if (ipMax >= 0.75) {            // Elevé = rouge
-      if (diameter >= 150){
-        fill(231, 125, 125, 180);
-      }
-      else {
-        fill(231, 125, 125);
-      }
-      
-    } else if (ipMax >= 0.5) {     // Moyen = orange
-        if (diameter >=150){
-          fill(248, 193, 129, 180);
+      // Définir la couleur en fonction de l'IP Max
+      let ipMax = data.getNum(i, 'IP');
+      if (ipMax >= 0.75) {                                // Elevé = rouge
+        if (diameter >= 150){ fill(231, 125, 125, 180); }
+        else { fill(231, 125, 125); }
         }
-        else {
-          fill(248, 193, 129)
+        else if (ipMax >= 0.5) {                         // Moyen = orange
+            if (diameter >=150) { fill(248, 193, 129, 180); }
+            else { fill(248, 193, 129); }
         }
-      
-    } else if (ipMax >= 0.25  || ipMax >= 0.01) {      // Faible = jaune
-      if (diameter >= 150) {
-        fill(243, 230, 105, 180);
+        else if (ipMax >= 0.25  || ipMax >= 0.01) {      // Faible = jaune
+            if (diameter >= 150) { fill(243, 230, 105, 180); }
+            else { fill(243, 230, 105); }
+        }
+        else if (ipMax <0.01){                           // Très faible = vert
+            if (diameter >= 150) { fill(181, 244, 179,180); }
+            else { fill(181, 244, 179); } 
+        }
       }
-      else {
-        fill(243, 230, 105)
+  
+      // Animation lorsque la souris passe sur une bulle
+      let d = dist(x, y, mouseX, mouseY);
+      if (d < radius/2) {
+        // Modifier la couleur de la bulle
+        fill(51, 204, 255);
+        circleSpeeds = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
       }
-      
-    }
-    
-    else if (ipMax <0.01){        // Très faible = vert
-      if (diameter >= 150){
-        fill(181, 244, 179,180)
-      }
-      else {
-        fill(181, 244, 179)
-      } 
-    }
-    
-    // Animation lorsque la souris passe sur une bulle
-    let d = dist(x, y, mouseX, mouseY);
-    if (d < radius/2) {
-      // Modifier la couleur de la bulle
-      fill(51, 204, 255);
-      circleSpeeds = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    }
-    
-    // Afficher les bulles
-    ellipse(x, y, radius);
 
-    // Anime la rotation des bulles sur l'orbite
-    for (let i = 0; i < numCircles; i++) {
-      circleAngles[i] += circleSpeeds[i];
+      // Afficher les bulles
+      ellipse(x, y, radius);
+
+      // Anime la rotation des bulles sur l'orbite
+      for (let i = 0; i < numCircles; i++) {
+        circleAngles[i] += circleSpeeds[i];
+      }
     }
-  }
 
 function cercleHover(cercle){    //Mettre en surbrillance les cercles hovered
   asteroid.fill(51, 204, 255);
@@ -164,4 +147,4 @@ function modifyCircles() {
     addedX += 1
     moveCircle = true;
   }
- }
+}
